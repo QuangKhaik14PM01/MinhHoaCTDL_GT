f01bd801a1a6b7a2e706139db5ddf612ae3a9dbb
Quangkhaik14PM01 <tranquangkhai2992002@gmail.com>
2021-09-24 19:43:21 +0700

    cow

---
+++ src/edu/giadinh/vn/tuan3/Cow.java
@@ -0,0 +1,18 @@
+package edu.giadinh.vn.tuan3;
+
+public class Cow {
+   
+    double weight; 
+
+    //hàm tạo
+    //devlopers tạo ra chứ không để java compiler tạo ra
+    Cow(){// hàm tạo do devlopers tạo ra
+
+    }
+
+    Cow(double w){
+    weight = w;
+
+    }
+
+}
