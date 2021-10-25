ae938f02614a79d8d0406ccc42a37d022bae61e0
Quangkhaik14PM01 <tranquangkhai2992002@gmail.com>
2021-10-25 11:10:25 +0700

    thi giữa kìa bài 1

---
+++ src/com/Khaitq20041110035/kiemtragiuaki/apple1.java
@@ -0,0 +1,48 @@
+package com.Khaitq20041110035.kiemtragiuaki;
+
+import java.util.Objects;
+
+public class apple1 {
+    Double maid;
+    Double khoiluong;
+    String mausac;
+
+    public apple1(){}
+
+    public apple1(Double maid, Double khoiluong,String mausac ){
+        this.maid = maid;
+        this.khoiluong = khoiluong;
+        this.mausac = mausac;
+
+    }
+    public Double getmaid() {
+        return maid;
+    }
+
+    public void setmaid(Double maid) {
+        this.maid = maid;
+    }
+
+    public Double getkhoiluong() {
+        return khoiluong;
+    }
+
+    public void setkhoiluong(Double khoiluongDouble) {
+        this.khoiluong = khoiluongDouble;
+    }
+
+    public String getmausac() {
+        return mausac;
+    }
+    public void setmausac(String mausac) {
+        this.mausac = mausac;
+    }
+   
+    
+    }
+
+
+
+
+    
+
