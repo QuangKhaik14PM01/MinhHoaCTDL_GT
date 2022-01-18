17d748bafa80e66f1640637d179951ce69ce7a6a
Quangkhaik14PM01 <tranquangkhai2992002@gmail.com>
2022-01-18 14:17:51 +0700

    buổi 2

---
+++ huongdoituong/src/tuan1/sinhvienTestDrive.java
@@ -0,0 +1,11 @@
+package tuan1;
+
+public class sinhvienTestDrive {
+
+   public static void main(String[] args) {
+    sinhvien khaisinhvien = new sinhvien();
+    khaisinhvien.hoc();
+    khaisinhvien.thi();
+    
+   }
+}
