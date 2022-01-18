da5f3f3762a77fd250614546ff2b6335dfb5d770
Quangkhaik14PM01 <tranquangkhai2992002@gmail.com>
2022-01-18 14:17:41 +0700

    buổi 2

---
+++ huongdoituong/src/tuan1/sinhvien.java
@@ -0,0 +1,24 @@
+package tuan1;
+
+public class sinhvien {
+    boolean gioitinh;
+    float tuoi;
+    String ten;
+    float diemtb;
+    String khoa;
+
+
+
+    void hoc(){
+        System.out.println("học");
+    }
+
+    void thi(){
+        System.out.println("thi");
+    }
+
+    void dangkymon(){
+        System.out.println("đăng ký môn");
+    }
+    
+}
