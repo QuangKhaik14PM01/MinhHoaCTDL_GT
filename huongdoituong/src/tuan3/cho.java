cb26c22b0834ce133b27bb0005a43ca96300f296
Quangkhaik14PM01 <tranquangkhai2992002@gmail.com>
2022-02-22 12:27:36 +0700

    tuần3

---
+++ huongdoituong/src/tuan3/cho.java
@@ -0,0 +1,23 @@
+package tuan3;
+
+public class cho {
+    int size;
+    String breed;
+    String name;
+
+    void bark(){
+        if(size>14){
+            System.out.println("ruff!");
+        }else{
+            System.out.println("yip");
+        }
+   
+    }
+    void setbigger(){
+        size+=5;
+    }
+    void run(){
+        System.out.println("running ");
+        this.bark();
+    }
+}
---
+++ huongdoituong/src/tuan3/choTestDirve.java
@@ -0,0 +1,10 @@
+package tuan3;
+
+public class choTestDirve {
+   public static void main(String[] args) {
+    cho cho1 =new cho();
+    cho1.bark();
+    cho1.setbigger();
+    cho1.run();
+   }
+}
