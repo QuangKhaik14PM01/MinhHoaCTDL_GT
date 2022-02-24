ba3c16116452156ce3de1343087c4141638c223f
Quangkhaik14PM01 <tranquangkhai2992002@gmail.com>
2022-02-24 13:09:48 +0700

    Hình

---
+++ huongdoituong/src/Shape/Shape.java
@@ -0,0 +1,23 @@
+package Shape;
+
+public class Shape {
+    int size;
+    String color;
+    String audio;
+    String name;
+
+    Shape(int s, String c, String a, String n){
+        size = s;
+        color = c;
+        audio = a;
+        name = n;
+    }
+    void XoayHinh(){
+
+    }
+
+    void TiengHinh(){
+        
+    }
+    
+}
---
+++ huongdoituong/src/Shape/ShapeTestDrive.java
@@ -0,0 +1,10 @@
+package Shape;
+
+public class ShapeTestDrive {
+    public static void main(String[] args) {
+        Shape shape1 = new Shape(10, "đỏ ", "rock ", "Hình Vuong ");
+
+        shape1.TiengHinh();
+        shape1.XoayHinh();
+    }
+}
